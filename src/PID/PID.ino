9bf040b3b23508303846cce1e84852eb9e045743
TheeBrainiacs <145912020+Kronbii@users.noreply.github.com>
2023-11-11 01:26:55 +0200

    Update PID

--- src/PID/PID.ino
+++ src/PID/PID.ino
@@ -1,4 +1,13 @@
+#include <Servo.h>
+
+//define servo motors
+Servo yawServo;
+Servo pitchServo;
+int yawPin; //add a value
+int pitchPin; //add a value
+
 //Variables to store the PID gains
+float kp, kd, ki;
 float kp1, kd1, ki1;
 float kp2, kd2, ki2;
 
@@ -18,17 +27,32 @@ double output;
 //error values
 double error, derivError, integError, prevError;
 
+//function definitions
 double PID_control(double error);
 void readSensors();
+void PID_control();
 
 void setup() {
   // Initialize gain values
+  kp = 1;
+  kd= 1;
+  ki = 1;
 
-  /*
-  kp = ;
-  kd = ;
-  ki = ;
-  */
+  kp1 = kp;
+  kd1 = kd;
+  ki1 = ki;
+
+  kp2 = kp;
+  kd2 = kd;
+  ki2 = ki;
+
+  //Attach servos
+  yawServo.attach(yawPin);
+  pitchServo.attach(pitchPin);
+
+  //Set servo motors to midpoint
+  yawServo.writeMicroseconds(1500);
+  pitchServo.writeMicroseconds(1500);
 
 }
 
@@ -44,7 +68,7 @@ void loop() {
 double PID_control(double error, double prevError){
   derivError = error - prevError;
   integError = error + integError;
-  output = kp*error + kd*derivError + ki*integError;
+  return output = kp*error + kd*derivError + ki*integError;
 }
 
 void readSensors(){
@@ -57,6 +81,14 @@ void readSensors(){
   //find the TF of the LDR depending on the lowest and maximum values of light
   //put the LDR in a voltage division circuit to convert resistave output to voltage
 
+}
+
+void PID_control(double output){
+  //1500 is midpoint, 1000 is fully ccw, 2000 is fully cw
+  //control servo motors (write.Microseconds)
+  yawServo.writeMicroseconds(1500 + output);
+  yawServo.writeMicroseconds(1500 + output);
+  
 }
 
 
