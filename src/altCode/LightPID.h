3d0f8d059221a12d3b0626f088e4c6bbf7f2ef07
TheeBrainiacs <145912020+Kronbii@users.noreply.github.com>
2023-11-12 00:46:25 +0200

    depression code at 1 am

---
+++ src/altCode/LightPID.h
@@ -0,0 +1,51 @@
+#include <Servo.h>
+
+class Yaw {
+  public:
+  double kp, kd, ki;
+  double integError, derivError, error, prevError;
+  Servo servo;
+  int servoPin;
+
+  // constructor
+  Yaw(double kp, double kd, double ki, int servoPin){
+    this->kp = kp;
+    this->kd = kd;
+    this->ki = ki;
+    integError = 0;
+    derivError = 0;
+    error = 0;
+    prevError = 0;
+    this->servoPin = servoPin;
+    servo.attach(servoPin);
+  }
+
+  double calculatePID(double error){
+    double output;
+    this->error = error;
+    derivError = this->error + prevError;
+    integError += this->error;
+    prevError = this->error;
+    return output = kp*this->error + kd*derivError + ki*integError;
+  }
+
+  void controlServo(double output){
+    output = int(output);
+    servo.writeMicroseconds(1500 + output);
+  }
+};
+
+class Pitch : public Yaw{
+  public:
+  pitch(double kp, double kd, double ki, int servoPin){
+    this->kp = kp;
+    this->kd = kd;
+    this->ki = ki;
+    integError = 0;
+    derivError = 0;
+    error = 0;
+    prevError = 0;
+    this->servoPin = servoPin;
+    servo.attach(servoPin);
+  }
+};
--- src/altCode/altCode.ino
+++ src/altCode/altCode.ino
@@ -1,4 +1,4 @@
- #include <Servo.h>
+#include <Servo.h>
 
 class Yaw {
   public:
@@ -29,32 +29,78 @@ class Yaw {
     return output = kp*this->error + kd*derivError + ki*integError;
   }
 
-  void controlServo(){
-    output = calculatePID();
+  void controlServo(double output){
     output = int(output);
     servo.writeMicroseconds(1500 + output);
   }
 };
 
+void readSensors();
+
 class Pitch : public Yaw{
+  public:
+  Pitch(double kp, double kd, double ki, int servoPin) : Yaw(kp, kd, ki, servoPin) {}
 };
-
-
 //////////////////////////////Sensor values/////////////////////////////////
 double s1,s2,s3,s4;
+double setpoint, feedback;
+double output;
 ////////////////////////////////////////////////////////////////////////////
+
+Yaw yaw(1, 1, 1, 9);
+Pitch pitch(1, 1, 1, 11);
+
 
 void readSensors();
 
 void setup() {
+  pinMode(9, OUTPUT);
+  pinMode(11, OUTPUT);
 
-  Yaw yaw(1, 1, 1, 9);
-  Pitch pitch(1, 1, 1, 11);
+  yaw.controlServo(0);
+  pitch.controlServo(0);
 
+  setpoint = 0;
+  feedback = 0;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  readSensors();
+
+  //update error values for yaw
+  if (s1 > s2){
+    setpoint = s1;
+    feedback = s2;
+  }
+  else if(s1 < s2){
+    setpoint = s2;
+    feedback = s1;
+  }
+
+  if (s3 > s4){
+    setpoint += s3;
+    feedback += s4;
+  }
+  else if(s3 < s4){
+    setpoint += s4;
+    feedback += s3;
+  }
+
+  yaw.prevError = yaw.error;
+  yaw.error = setpoint - feedback;
+
+  output = yaw.calculatePID(yaw.error);
+  yaw.controlServo(output);
+
+  readSensors();
+
+  setpoint = s3 + s4;
+  feedback = s1 + s2;
+  pitch.prevError = pitch.error;
+  pitch.error = setpoint - feedback;
+
+  output = pitch.calculatePID(pitch.error);
+  pitch.controlServo(output);
 
 }
 
