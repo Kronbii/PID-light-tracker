b6162b679dcb51837cbb8dcaf509838b4adbeefd
TheeBrainiacs <145912020+Kronbii@users.noreply.github.com>
2023-11-22 00:08:11 +0200

    e3id ma3 homsi w abed w depresssed

--- src/altCode/LightPID.h
+++ src/altCode/LightPID.h
@@ -4,11 +4,9 @@ class Yaw {
   public:
   double kp, kd, ki;
   double integError, derivError, error, prevError;
-  //Servo servo;
-  //int servoPin;
 
   // constructor
-  Yaw(double kp, double kd, double ki, int servoPin){
+  Yaw(double kp, double kd, double ki){
     this->kp = kp;
     this->kd = kd;
     this->ki = ki;
@@ -16,8 +14,6 @@ class Yaw {
     derivError = 0;
     error = 0;
     prevError = 0;
-    //this->servoPin = servoPin;
-    //servo.attach(servoPin);
   }
 
   double calculatePID(double error){
@@ -27,21 +23,14 @@ class Yaw {
     derivError = this->error - prevError;
     integError += this->error;
     output = kp*this->error + kd*derivError + ki*integError;
-    output = int(output);
     if (output > 90) output = 90;
     if (output < -90) output = -90;
     return output;
   }
-/*
-  void controlServo(double output){
-    output = int(output);
-    servo.writeMicroseconds(1500 + output);
-  }
-  */
 };
 
 
 class Pitch : public Yaw{
   public:
-  Pitch(double kp, double kd, double ki, int servoPin) : Yaw(kp, kd, ki, servoPin) {}
+  Pitch(double kp, double kd, double ki) : Yaw(kp, kd, ki) {}
 };
--- src/altCode/altCode.ino
+++ src/altCode/altCode.ino
@@ -3,6 +3,8 @@
 
 Servo yawServo;
 Servo pitchServo;
+const int yawPin = 9;
+const int pitchPin = 10;
 
 //////////////////////////////Sensor values/////////////////////////////////
 double s1,s2,s3,s4;
@@ -11,8 +13,8 @@ double output;
 double curError;
 ////////////////////////////////////////////////////////////////////////////
 
-Yaw yaw(30, 000, 0.0, 9);
-Pitch pitch(100, 000, 0, 10);
+Yaw yaw(30, 000, 0.0);
+Pitch pitch(30, 000, 0);
 
 
 void readSensors();
@@ -20,15 +22,15 @@ void readSensors();
 void setup() {
   Serial.begin(9600);
 
-  pinMode(9, OUTPUT);
-  pinMode(10, OUTPUT);
+  pinMode(yawPin, OUTPUT);
+  pinMode(pitchPin, OUTPUT);
 
   setpoint = 0;
   feedback = 0;
   curError = 0;
 
-  yawServo.attach(9);
-  pitchServo.attach(10);
+  yawServo.attach(yawPin);
+  pitchServo.attach(pitchPin);
 
   yawServo.write(0);
   pitchServo.write(90);
@@ -38,37 +40,41 @@ void loop() {
   readSensors();
 
   //update error values for yaw
-  if ((s1 + s3) > (s2 + s4)){
     setpoint = (s1 + s3)/2.00;
     feedback = (s2 + s4)/2.00;
-  }
-  else {
-    setpoint = (s2 + s4)/2.00;
-    feedback = (s1 + s3)/2.00;
-  }
-
+    
   //calculating error
   curError = setpoint - feedback;
 
+  /*
+  Serial.println(curError);
+  delay(1000);
+  */
+  
   output = yaw.calculatePID(curError);
+/*
+  Serial.print("output = ");
+  Serial.println(output);
+  Serial.println("");
+  delay(1000);
+  */
   yawServo.write(90 + output);
  
   //pitch control
   readSensors();
   
-  if((s1 + s2) > (s3 + s4))
-  {
-  setpoint = (s3 + s4)/2.00;
-  feedback = (s1 + s2)/2.00;
-  }
-  else{
-    setpoint = (s1 + s2)/2.00;
-    feedback = (s3 + s4)/2.00;
-  }
+  setpoint = (s1 + s2)/2.00;
+  feedback = (s3 + s4)/2.00;
   
   curError = setpoint - feedback;
 
   output = pitch.calculatePID(curError);
+
+
+  Serial.print("output = ");
+  Serial.println(output);
+  delay(1000);
+  
   
   if (output > 25) output = 25;
   if (output < -25) output = -25;
@@ -77,20 +83,24 @@ void loop() {
 }
 
 void readSensors(){
-  s2 = analogRead(A0);
-  s4 = analogRead(A1);
-  s1 = analogRead(A2);
-  s3 = analogRead(A3);
+  s1 = analogRead(A0);
+  s2 = analogRead(A1);
+  s3 = analogRead(A2);
+  s4 = analogRead(A3);
+
   
   s1 = s1 * 5.00/1023;
   s2 = s2 * 5.00/1023;
   s3 = s3 * 5.00/1023;
   s4 = s4 * 5.00/1023;
 
+/*
   Serial.println(s1);
    Serial.println(s2);
     Serial.println(s3);
      Serial.println(s4);
       Serial.println("");
       delay(1000);
+      */
+
 }
