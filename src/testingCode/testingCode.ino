61bb7cdfd1307c65c5b5759c17296417870ea316
TheeBrainiacs <145912020+Kronbii@users.noreply.github.com>
2023-11-30 00:23:02 +0200

    ZABATTTTT

--- src/testingCode/testingCode.ino
+++ src/testingCode/testingCode.ino
@@ -13,8 +13,8 @@ double output;
 double curError;
 ////////////////////////////////////////////////////////////////////////////
 
-Yaw yaw(0.3, 000, 0.0, -90, 90);
-Pitch pitch(60 , 1500, 0, -70, 70);
+Yaw yaw(0.3, 0, 0, -90, 90);
+Pitch pitch(0.5 , 0.01, 0, -45, 45);
 
 void setup() {
   Serial.begin(9600);
@@ -44,17 +44,17 @@ void loop() {
 
   output = pitch.calculatePID(curError);
 
-/*  
+/*
   Serial.print("output = ");
   Serial.println(output);
-  */
+*/
   
   pitchServo.write(90 - output);
 }
 
 void readSensors(){
   topl = analogRead(A0);
-  topr = analogRead(A1);
+  topr = analogRead(A5); //change to A1
   botl = analogRead(A2);
   botr = analogRead(A3);
 
@@ -65,13 +65,14 @@ void readSensors(){
   topl = topl * 5.00/1023;
 */
 
- 
+ /*
   Serial.println(botr);
    Serial.println(botl);
     Serial.println(topr);
      Serial.println(topl);
       Serial.println("");
-      delay(1000);
+      delay(500);
+      */
 }
 
 /////FILLER FUNCTION///////
@@ -84,16 +85,8 @@ void yawCode(){
     
   //calculating error
   curError = setpoint - feedback;
-
-  Serial.println(curError);
-  delay(1000);
   
   output = yaw.calculatePID(curError);
-
-  Serial.print("output = ");
-  Serial.println(output);
-  Serial.println("");
-  delay(1000);
  
   yawServo.write(90 + output);
 }
