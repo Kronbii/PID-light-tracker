824a1a4a84c7adec11eea791363a71714a506c9d
TheeBrainiacs <145912020+Kronbii@users.noreply.github.com>
2023-11-30 17:45:36 +0200

    Updating pitch PID

---
+++ src/PitchPID/PitchPID.ino
@@ -0,0 +1,95 @@
+//////////////////////////////Libraries Used/////////////////////////////////
+  #include "LightPID.h"
+  #include <Servo.h>
+////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////Servo Variables/////////////////////////////////
+  Servo pitchServo;
+  const int pitchPin = 6;
+/////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////Sensor Values/////////////////////////////////
+  double botr, botl, topr, topl;
+  double setpoint, feedback;
+  double output;
+  double curError;
+  int initAngle =0;
+  int tiltAngle =0;
+////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////PID Objects///////////////////////////////////
+  Pitch pitch(0.07 , 0.00018, 0.002, -70, 55);
+////////////////////////////////////////////////////////////////////////////
+
+void setup() {
+  //Set servo pin as output
+  pinMode(pitchPin, OUTPUT);
+
+  //Initialize values of error and input needed for PID
+  setpoint = 0;
+  feedback = 0;
+  curError = 0;
+
+  //Attach servo pin
+  pitchServo.attach(pitchPin);
+
+  //Set the servo initially vertical (90 degrees)
+  pitchServo.write(90);
+}
+
+void loop() {
+  //Read values of sensors and store them
+  readSensors();
+  
+  //Calculating the value of setpoint and feedback based on the positions of the sensor positions
+  setpoint = (botr + botl)/2.00;
+  feedback = (topr + topl)/2.00;
+  
+  //Calculate the value of the PID error
+  curError = setpoint - feedback;
+
+  //Get the value of the output using the PID calculate method
+  output = pitch.calculatePID(curError);
+
+  //Move the servo to the caluclated PID angle
+  pitchServo.write(90 - output);
+}
+
+
+//////////////////////////////Functions///////////////////////////////////
+  //Function to read values of sensors
+  void readSensors(){
+    topl = analogRead(A0);
+    topr = analogRead(A5); //change to A1
+    botl = analogRead(A2);
+    botr = analogRead(A3);
+  }
+
+  void pitchScan(int& initAngle){
+    //initial reading of sensors
+    readSensors();
+  
+    //variable to store reading during runtime
+    double temp;
+
+    //variable to store initial reading
+    double init = botr + topr + topl + botl;
+  
+    for (int i=0; i<180; i++){
+      //write servo angle
+      pitch.write(i);
+
+      //read sensors to find the intensity of light
+      readSensors();
+
+      //calculate intensit of light
+      temp = botr + topr + topl + botl;
+
+      //update the maximum reading of sensors
+      if (temp > init) initAngle = i;
+
+      //control speed of servo
+      delay(15);
+    }
+  }
+////////////////////////////////////////////////////////////////////////////
